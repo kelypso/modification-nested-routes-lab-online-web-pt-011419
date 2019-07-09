f233fb3411a560f48c3f370b956025841e3498e8
Kelsey Low <kklowlow@gmail.com>
2019-07-09 00:47:20 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,11 +1,9 @@
 module SongsHelper
-
-  def artist_select(song)
-    if song.artist.nil?
-      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+  def artist_select(artist, song)
+    if artist
+      artist.name
     else
-      hidden_field_tag "song[artist_id]", song.artist_id
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     end
   end
-
 end
